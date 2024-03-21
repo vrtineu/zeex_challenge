be326d68a139adbbc5442896ca47b575a1b769c7
vrtineu <vininro@gmail.com>
2024-03-21 20:21:54 -0300

    Add partner creation from JSON in seeds.exs

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,16 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+json = File.read!("challenge/pdvs.json")
+%{"pdvs" => pdvs} = Jason.decode!(json)
+
+for pdv <- pdvs do
+  Zeex.Store.create_partner(%{
+    address: Geo.JSON.decode(pdv["address"]),
+    coverage_area: Geo.JSON.decode(pdv["coverageArea"]),
+    document: pdv["document"],
+    owner_name: pdv["ownerName"],
+    trading_name: pdv["tradingName"]
+  })
+end
